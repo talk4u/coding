dd59145704f2e0e245e4f62204b136b432b91b32
wdyd2004 <wdyd2004@naver.com>
2018-05-24 14:12:29 +0900

    fix: remove submission_data and add submission_id in rank detail #30

--- api/serializers.py
+++ api/serializers.py
@@ -93,14 +93,11 @@ class ProblemRankSerializer(serializers.ModelSerializer):
     user_name = serializers.CharField(
         source='submission.user.username'
     )
-    submission_data = serializers.CharField(
-        source='submission.submission_data'
-    )
 
     class Meta:
         model = models.JudgeResult
         fields = ('user_name', 'memory_used_bytes', 'time_elapsed_seconds',
-                  'code_size', 'submission_data', 'created_at')
+                  'code_size', 'submission_id', 'created_at')
 
 
 class SubmissionSerializer(serializers.ModelSerializer):
