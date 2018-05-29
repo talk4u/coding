5025531e446b8d502b9263834320221c8d5fbb7e
wdyd2004 <wdyd2004@naver.com>
2018-05-29 20:29:28 +0900

    feat: add submission detail serializer for treadmil #33

--- api/s3.py
+++ api/s3.py
@@ -1,5 +1,6 @@
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+from boto.s3.prefix import Prefix
 from django.conf import settings
 
 
@@ -27,3 +28,25 @@ def generate_private_url(key):
 def get_submission_path(instance, filename):
     extension = filename.split(".")[-1]
     return 'submission/{}/code.{}'.format(instance.id, extension)
+
+
+def get_files_in_directory(prefix):
+    prefix += '/'
+    s3 = S3Connection(settings.AWS_ACCESS_KEY_ID,
+                      settings.AWS_SECRET_ACCESS_KEY,
+                      is_secure=True)
+    bucket = s3.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+
+    out = {
+        'prefix': prefix,
+        'files': [],
+        'directories': []
+    }
+
+    for key in bucket.get_all_keys(prefix=prefix):
+        if isinstance(key, Prefix):
+            out['directories'].append(key.name)
+        elif isinstance(key, Key) and key.name != prefix:
+            out['files'].append(key.name)
+
+    return out['files']
