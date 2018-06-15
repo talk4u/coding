1f31a33cd2cfeef4baea950ebfe8c6bf516ecdc3
wdyd2004 <wdyd2004@naver.com>
2018-06-15 14:25:26 +0900

    fix: add parents lookup filter in Judge Result API

--- api/views.py
+++ api/views.py
@@ -86,6 +86,7 @@ class JudgeResultViewSet(NestedViewSetMixin, ModelViewSet):
         user = self.request.user
         qs = models.JudgeResult.objects.all() if is_instructor(user) \
             else models.JudgeResult.objects.filter(submission__user=user)
+        qs = self.filter_queryset_by_parents_lookups(qs)
         results = get_latest_judge_result_queryset(qs)
         return results.order_by('-created_at')
 
